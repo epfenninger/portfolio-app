854fcbb2012ade3c0bd087a614bc04f3993eec66
Ethan Pfenninger <epfenni@gmail.com>
2022-01-03 14:59:32 -0500

    Making the footer actually work (not there yet)

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,45 @@
 import React, { Component } from "react";
+import Card from "@mui/material/Card";
+import CardMedia from "@mui/material/Card";
+
+const footerStyle = {
+  backgroundColor: "rgba(0, 0, 0, .5)",
+  height: "3vw",
+  maxHeight: "300px",
+  align: "center",
+};
+
+const textStyle = {
+  color: "white",
+  marginLeft: "10vw",
+};
+
+const imageStyle = {
+  height: "10px",
+  width: "10px",
+};
 
 class Footer extends React.Component {
   render() {
     return (
-      <div>
-        <div className="footer">
-          <footer class="py-5 bg-dark fixed-bottom">
-            <div class="container">
-              <p class="m-0 text-center text-white">
-                Copyright &copy; Your Website 2020
-              </p>
-            </div>
-          </footer>
-        </div>
+      <div
+        style={{
+          position: "absolute",
+          left: "0",
+          bottom: "0",
+          right: "0",
+          maxHeight: "30px",
+        }}
+      >
+        <Card style={footerStyle}>
+          <h5 style={textStyle}>Built by Ethan Pfenninger</h5>
+          <CardMedia
+            href="linkedin.com/in/ethan-pfenninger-84437889"
+            component="img"
+            height="10px"
+            image={require("../images/linkedin.png")}
+          />
+        </Card>
       </div>
     );
   }
