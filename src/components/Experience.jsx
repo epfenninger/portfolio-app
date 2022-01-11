8a115855d30f8bb624041446b9fb4a3b280559c8
Ethan Pfenninger <epfenni@gmail.com>
2022-01-11 15:00:15 -0500

    Basically complete redesign.

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,57 +1,14 @@
-import React, { Component } from "react";
-import { Chrono } from "react-chrono";
+// @flow
+import React from "react";
+import styled from "styled-components";
+import { Controller, Scene } from "react-scrollmagic";
+import { Tween, Timeline } from "react-gsap";
 
-const items = [
-  {
-    title: "Expected May 2022",
-    cardTitle: "Masters in Educational Technology and Curriculum Design",
-    cardDetailedText:
-      "Today I'm looking to leverage my experience in Instructional Design, Educational Best-Practice, and LMS Administration to move into the corporate arena",
-  },
-  {
-    title: "January 2022",
-    cardTitle: "Google Project Management and Data Analytics Professional",
-    cardDetailedText:
-      "Today I'm looking to leverage my experience in Instructional Design, Educational Best-Practice, and LMS Administration to move into the corporate arena",
-  },
-  {
-    title: "August 2019 - Present",
-    cardTitle: "Alton Central School",
-    cardDetailedText: "ACS Tag Text",
-  },
-  {
-    title: "August 2017 - August 2019",
-    cardTitle: "Holderness",
-    cardDetailedText: "Holderness Text",
-  },
-  {
-    title: "May 2015",
-    cardTitle: "Graduated from UNH - B.S. Information Technology",
-    cardDetailedText: "UNH TagLine",
-  },
-  {
-    title: "March 2015 - August 2017",
-    cardTitle: "Tilton School",
-    cardDetailedText: "Tilton TagLine",
-  },
-];
 class Experience extends React.Component {
   render() {
     return (
-      <div
-        className="text-center"
-        style={{
-          display: "flex",
-          width: "90vh",
-          margin: "auto",
-          height: "90vh",
-        }}
-      >
-        <Chrono
-          items={items}
-          mode="VERTICAL_ALTERNATING"
-          scrollable={{ scrollbar: false }}
-        />
+      <div className="experience">
+        <h2>I've done a few things over the past couple years....</h2>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,52 +1,117 @@
-import React from "react";
+import React, { Component } from "react";
+import { Controller, Scene } from "react-scrollmagic";
+import styled from "styled-components";
 import ReactDOM from "react-dom";
 import "./index.css";
-import reportWebVitals from "./reportWebVitals";
-import "bootstrap/dist/css/bootstrap.css";
-import "/node_modules/react-grid-layout/css/styles.css";
-import "/node_modules/react-resizable/css/styles.css";
-import logo from "./logo.svg";
-import "./App.css";
-import NavBar from "./components/navbar";
-import Tiles from "./components/TileGrid.jsx";
-import background from "./images/background.jpg";
+import TileGrid from "./components/TileGrid";
+import { Tween, Timeline } from "react-gsap";
+import backgroundvideo from "./images/background.mp4";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import Footer from "./components/Footer";
-import About from "./components/About";
 import Contact from "./components/Contact";
 import Experience from "./components/Experience";
 import Resume from "./components/Resume";
 import Portfolio from "./components/Portfolio";
+import NavBar from "./components/navbar";
+
+const SectionWipesStyled = styled.div`
+  overflow: hidden;
+  .panel {
+    height: 100vh;
+    width: 100%;
+  }
+
+  .panel span {
+    position: relative;
+    overflow: visible;
+    top: 50%;
+  }
+
+  .navbar.navbartext {
+  margin: "1rem",
+  textDecoration: "none",
+  color: "white",
+  fontWeight: "bold",
+  variant: "outlined",
+  fontSize: "1.3vw",
+  }
+
+  .panel.home {
+    background-color: #121212;
+  }
+
+  .panel.tilegrid {
+    background-color: #212121;
+    height: 110vh;
+
+    .skills {
+      position: relative;
+      top: -40vh;
+    }
+  }
+
+  .panel.experience {
+    background-color: #eeeeee;
+    height: 110vh;
+  }
+
+  .panel.portfolio {
+    background-color: #616161;
+    height: 110vh;
+  }
+
+  .panel.contact {
+    background-color: #3b3b3b;
+    height: 110vh;
+  }
+`;
 
 ReactDOM.render(
   <React.StrictMode>
-    <div
-      style={{
-        backgroundImage: `url(${background})`,
-        height: "100%",
-        backgroundSize: "cover",
-        height: "100vh",
-      }}
-    >
-      <Router>
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/experience" element={<Experience />} />
-          <Route path="/portfolio" element={<Portfolio />} />
-          <Route path="/resume" element={<Resume />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </Router>
-    </div>
-    <Footer />
+    <SectionWipesStyled>
+      <Controller globalSceneOptions={{ triggerHook: "onLeave" }}>
+        <div className="navbar">
+          <NavBar />
+        </div>
+
+        <Scene pin>
+          <div className="panel home">
+            <span>
+              <Home />
+            </span>
+          </div>
+        </Scene>
+        <Scene pin>
+          <div className="panel tilegrid">
+            <span>
+              <TileGrid />
+            </span>
+          </div>
+        </Scene>
+        <Scene pin>
+          <div className="panel experience">
+            <span>
+              <Experience />
+            </span>
+          </div>
+        </Scene>
+        <Scene pin>
+          <div className="panel portfolio">
+            <span>
+              <Portfolio />
+            </span>
+          </div>
+        </Scene>
+        <Scene pin>
+          <div className="panel contact">
+            <span>
+              <Contact />
+            </span>
+          </div>
+        </Scene>
+      </Controller>
+    </SectionWipesStyled>
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
