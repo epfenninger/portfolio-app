a86aa3f87d5d43bae7c1ee1c7713490c5699cce1
epfenninger <epfenni@gmail.com>
2022-01-17 09:49:07 -0500

    Actually have a portfolio!

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import PortfolioCard from "./PortfolioCard";
 import Grid from "@mui/material/Grid";
-import image from "../images/background.jpg";
+import contact from "../data/content.json";
 
 class Portfolio extends React.Component {
   state = {};
@@ -19,12 +19,118 @@ class Portfolio extends React.Component {
         display="flex"
         marginRight="auto"
         marginLeft="auto"
+        bottom="10vh"
+        position="relative"
       >
+        <Grid item xs={12}>
+          <h1
+            style={{
+              marginLeft: "auto",
+              justifyContent: "center",
+              display: "flex",
+              fontSize: "2.5em",
+            }}
+          >
+            Portfolio
+          </h1>
+        </Grid>
+        <Grid item xs={4}>
+          <PortfolioCard
+            title="Risk Assessment when Winter Hiking in the White Mountains"
+            skills="Captivate, L&D"
+            imgSrc={require("../images/riskassessment.jpg")}
+            content={<img src={require("../images/tensorflow_lite.png")} />}
+            textColor={"#FFFFFF"}
+          />
+        </Grid>
+        <Grid item xs={4}>
+          <PortfolioCard
+            title="Comparing SCORM and xAPI"
+            skills="L&D, API, Camtasia"
+            imgSrc={require("../images/scormxapi.png")}
+            content={<img src={require("../images/tensorflow_lite.png")} />}
+            textColor={"black"}
+          />
+        </Grid>
         <Grid item xs={4}>
           <PortfolioCard
             title="Integrating Tensorflow Object detection via API"
-            imgSrc={require("../images/background.jpg")}
-            content={<img src={require("../images/background.jpg")} />}
+            skills="API, ML, Python"
+            imgSrc={require("../images/tensorflow_lite.png")}
+            content=""
+            textColor={"black"}
+            imgFit={"scale-down"}
+          />
+        </Grid>
+        <Grid item xs={4}>
+          <PortfolioCard
+            title="Integrating personalized domains with Google Sites"
+            skills="Camtasia, L&D, Microlearning"
+            imgSrc={require("../images/googledomain.png")}
+            imgFit={"scale-down"}
+            content="Very short video I put out for the L&D community on how to use Google Domains to personalize the url of their Google Site"
+            iFrameWidth={"800"}
+            iFrameHeight={"560"}
+            iFrameVideo={"https://www.youtube.com/embed/ZSS0ztftHpg"}
+            iFrameAllow={
+              "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            }
+            textColor={"black"}
+          />
+        </Grid>
+        <Grid item xs={4}>
+          <PortfolioCard
+            title="Running Data Analysis utilizing an undocumented API"
+            skills="API, Data Analysis, Python"
+            imgSrc={require("../images/DataAnalysis.jpg")}
+            content={<img src={require("../images/tensorflow_lite.png")} />}
+            textColor={"black"}
+          />
+        </Grid>
+        <Grid item xs={4}>
+          <PortfolioCard
+            title="Reverse Engineering undocumented modbus platform"
+            skills="API, Data Analysis, Python"
+            imgSrc={require("../images/modbus.png")}
+            content={<img src={require("../images/modbus.png")} />}
+            textColor={"black"}
+            imgFit={"scale-down"}
+          />
+        </Grid>
+        <Grid item xs={4}>
+          <PortfolioCard
+            title="Systems Integration for Smart-Van Build"
+            skills="Python, API, Data Analytics, Camtasia"
+            imgSrc={""}
+            content={<img src={require("../images/tensorflow_lite.png")} />}
+            textColor={"black"}
+          />
+        </Grid>
+        <Grid item xs={4}>
+          <PortfolioCard
+            title="Van Build!"
+            skills="Fun"
+            imgSrc={require("../images/van.jpg")}
+            content={<img src={require("../images/tensorflow_lite.png")} />}
+            textColor={"black"}
+          />
+        </Grid>
+        <Grid item xs={4}>
+          <PortfolioCard
+            title="Managing G-Suite via GAM and Google AppScript"
+            skills="Javascript, Systems Admin"
+            imgSrc={require("../images/gsuite.jpg")}
+            content={<img src={require("../images/tensorflow_lite.png")} />}
+            textColor={"black"}
+          />
+        </Grid>
+        <Grid item xs={4}>
+          <PortfolioCard
+            title=""
+            skills="Everything"
+            imgSrc={require("../images/github.png")}
+            content={<img src={require("../images/tensorflow_lite.png")} />}
+            textColor={"black"}
           />
         </Grid>
       </Grid>
